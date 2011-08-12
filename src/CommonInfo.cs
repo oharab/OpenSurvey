f61d1da57f957aec657c4853ad67fb3f3324ead8
Ben O'Hara <bpo@robotparade.co.uk>
2011-08-12 14:05:10 +0100

    Seperated common assembly information into CommonInfo.cs

---
+++ src/CommonInfo.cs
@@ -0,0 +1,10 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyVersion("0.0.1.*")]
+[assembly: AssemblyFileVersion("0.0.1.0")]
+[assembly: AssemblyCompany("robotparade.co.uk")]
+[assembly: AssemblyProduct("OpenSurvey")]
+[assembly: AssemblyCopyright("Copyright © robotparade.co.uk 2011")]
+[assembly: ComVisible(false)]
--- src/Core/Properties/AssemblyInfo.cs
+++ src/Core/Properties/AssemblyInfo.cs
@@ -2,35 +2,6 @@
 using System.Runtime.CompilerServices;
 using System.Runtime.InteropServices;
 
-// General Information about an assembly is controlled through the following 
-// set of attributes. Change these attribute values to modify the information
-// associated with an assembly.
-[assembly: AssemblyTitle("Core")]
-[assembly: AssemblyDescription("")]
-[assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("West Yorkshire Police")]
-[assembly: AssemblyProduct("Core")]
-[assembly: AssemblyCopyright("Copyright © West Yorkshire Police 2011")]
-[assembly: AssemblyTrademark("")]
-[assembly: AssemblyCulture("")]
-
-// Setting ComVisible to false makes the types in this assembly not visible 
-// to COM components.  If you need to access a type in this assembly from 
-// COM, set the ComVisible attribute to true on that type.
-[assembly: ComVisible(false)]
-
-// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: AssemblyTitle("OpenSurvey.Core")]
+[assembly: AssemblyDescription("Core models and functionality for OpenSurvey")]
 [assembly: Guid("bdd68f9b-cef6-4c00-ae48-6dfdc957cfd9")]
-
-// Version information for an assembly consists of the following four values:
-//
-//      Major Version
-//      Minor Version 
-//      Build Number
-//      Revision
-//
-// You can specify all the values or you can default the Build and Revision Numbers 
-// by using the '*' as shown below:
-// [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.0.0.0")]
-[assembly: AssemblyFileVersion("1.0.0.0")]
